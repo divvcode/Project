a9ac4fe68b92a86846ee26a73b0accaf5531b017
divvcode <divyasanjay8@gmail.com>
2023-05-09 20:24:05 +0530

    second commit

---
+++ AccessSpecifiers1.java
@@ -0,0 +1,24 @@
+package project2;
+class defAccessSpecifier
+{ 
+  void display() 
+     { 
+         System.out.println("You are using defalut access specifier"); 
+     } 
+} 
+
+
+public class AccessSpecifiers1 {
+
+	public static void main(String[] args) {
+		
+			//default
+			System.out.println("Dafault Access Specifier");
+			defAccessSpecifier obj = new defAccessSpecifier(); 		  
+	        obj.display(); 
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AccessSpecifiers2.java
@@ -0,0 +1,17 @@
+package project2;
+class priaccessspecifier 
+{ 
+} 
+
+
+public class AccessSpecifiers2 {
+
+	public static void main(String[] args) {
+		//private
+				System.out.println("Private Access Specifier");
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
